c49f3569a593f27f9333fb7115c8217f10e6a307
Kasane <hndseo205@gmail.com>
2017-06-06 15:47:51 +0700

    unit8

---
+++ Unit8-HND/js/main.js
@@ -0,0 +1,43 @@
+var s = document.getElementById('result')
+function click(cal) {
+	var idCal = document.getElementById(cal);
+	idCal.addEventListener("click",function () {
+		var a = parseInt(document.getElementById('value1').value);
+		var b = parseInt(document.getElementById('value2').value);
+			if(cal == 'sum') {
+				var result = a+b;
+				s.value = result;
+			}
+			else if(cal == 'sub') {
+				var result = a-b;
+				s.value = result;
+			}
+			else if(cal == 'multi') {
+				var result = a*b;
+				s.value = result;
+			}
+			else if(cal == 'divide') {
+				var result = a/b;
+				try {
+					if(b==0) {
+						throw("error by divide zero")
+					}
+				}
+				catch (err) {
+					alert('error by divide zero');
+					result ="";
+				}
+				finally {
+					s.value = result;
+				}
+			}
+
+		});
+}
+click('sum');
+click('sub');
+click('multi');
+click('divide');
+
+
+
