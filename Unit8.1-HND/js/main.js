7523cdee407be3af8cfe18ade3adbac311b0fed3
Kasane <hndseo205@gmail.com>
2017-06-14 21:01:22 +0700

    update

---
+++ Unit8.1-HND/js/main.js
@@ -0,0 +1,18 @@
+function getText(){
+	var rT = document.getElementById('value3').value;
+	var text = document.getElementById('result-text');
+	var number = /[0-9]/g;
+	var resultNumber = rT.match(number);
+	var sumNumber = 0;
+	console.log(resultNumber);
+	for ( var i =0; i<resultNumber.length; i++) {
+		sumNumber += parseInt(resultNumber[i]);
+	}
+	text.innerHTML = sumNumber;
+}
+
+var sN = document.getElementById('sum-number');
+sN.addEventListener('click', getText);
+
+
+
